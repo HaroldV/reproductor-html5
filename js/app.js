235141e939726771364247cf0265727de4a66991
Harold <haroldestebanvillalobos@gmail.com>
2016-10-03 22:28:11 -0400

    ultimo commit

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
 $(document).ready(function(){
+	initPlayer();
 	getSongs();	
 });
 
 var audio = document.getElementById('player');
 var music;
 
-console.log(audio);
+function initPlayer(){
+	$('#shuffle').click(function(){
+		$('#playlist').empty();
+		console.log(shuffle(music.songs));
+		genList(music);
+		playSong(0);
+	});
+}
+
 function getSongs(){
 	
 	$.getJSON('js/app.json', function(mjson) {
@@ -13,12 +22,10 @@ function getSongs(){
 		genList(music);
   		console.log("json recibido 2:",music);
 	});	
-
 }
 
 function playSong(id){
-	var long = music;
-
+	var long = music.songs;		
 	if(id >= long.length){
 		console.log('se acabaron las canciones');
 		audio.pause();	
@@ -29,7 +36,6 @@ function playSong(id){
 		console.log('Hay mas canciones');
 		scheduleSong(id);
 	}
-
 }
 
 function genList(music){		
@@ -48,8 +54,15 @@ function genList(music){
 
 function scheduleSong(id){
 	audio.onended = function(){
-		console.log('termino la cancion');
+		console.log('termino la cancion ',id);
 		playSong(parseInt(id)+1);
 	}
+}
+
+function shuffle(array){
+
+	for (var random, temp, position = array.length; position; random = Math.floor(Math.random()*position), temp = array[--position],array[position]=array[random],array[random]= temp);
+		return array;		
+
 }
 
